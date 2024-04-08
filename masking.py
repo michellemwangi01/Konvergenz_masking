1207f6dff0be7d7760aab8f84f46d075ec668e83
michellemwangi01 <michellemwangim@gmail.com>
2024-04-08 10:54:51 +0300

    first commit

---
+++ masking.py
@@ -0,0 +1,35 @@
+def mask_func(person):
+    masked_person = {}  # Initialize a new dictionary for the masked values
+
+    for key in person:
+        if key in ['first_name', 'last_name', 'date_of_birth', 'email_address']:  
+            word = person[key]
+            masked_words = []
+
+            
+            word_list = list(word)
+            for i in range(len(word_list)):
+                if i > 1:
+                    word_list[i] = '*'  
+            masked_word = ''.join(word_list)
+            masked_words.append(masked_word)
+
+            masked_string = ' '.join(masked_words)
+            masked_person[key] = masked_string  
+        else:
+            masked_person[key] = person[key]  
+
+    return masked_person
+
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'date_of_birth': '1990-05-15',
+    'email_address': 'john@mail.com',
+}
+
+masked_person = mask_func(person)
+
+print("Masked Person:", masked_person)
+
+
